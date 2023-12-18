becec6ee7a18e19e1bbc27af9dec974a08df6432
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2023-12-18 11:34:30 +0100

    eliminar funcion

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -3,18 +3,5 @@ public class BinaryToDecimal {
         System.out.println("Hello, World!");
     }
 
-    public static int BinaryToDecimal( String binaryString){
-        int decimalValue = 0;
- 
-        int len = binaryString.length();
-
-        for(int i=0; i< len; i++){
-            int digit = Character.getNumericValue(binaryString.charAt(i));
-            int power = len - i -1;
-
-            decimalValue = decimalValue * (int) (digit * Math.pow(2, power));
-        }
-
-        return decimalValue;
-    }
+    
 }

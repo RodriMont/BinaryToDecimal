d03d8c45e64987e3b3efb33803b4456069d4a563
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2023-12-18 11:31:21 +0100

    sviluppi funcione conversione

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -2,4 +2,19 @@ public class BinaryToDecimal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static int BinaryToDecimal( String binaryString){
+        int decimalValue = 0;
+ 
+        int len = binaryString.length();
+
+        for(int i=0; i< len; i++){
+            int digit = Character.getNumericValue(binaryString.charAt(i));
+            int power = len - i -1;
+
+            decimalValue = decimalValue * (int) (digit * Math.pow(2, power));
+        }
+
+        return decimalValue;
+    }
 }

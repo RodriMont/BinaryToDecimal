ed75ec7c293aea51e129568a804b57851fb2af11
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2023-12-18 11:03:38 +0100

    resolved bug

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class BinaryToDecimal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci una stringa: ");
+
+        String binaryString = scanner.nextLine();
+
+        int valoreDecimale = 0;
+
+        System.out.println("Valore decimale: " + valoreDecimale);
+
+        scanner.close();
     }
 }
